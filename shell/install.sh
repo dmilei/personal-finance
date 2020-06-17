e56d0ed34d0549c1babc7b87dc5d7daa9418aaf0
mileid <daniel.milei@stylersonline.com>
2020-06-17 12:38:11 +0200

    Include and config Laravel Passport

--- shell/install.sh
+++ shell/install.sh
@@ -13,7 +13,5 @@ docker-compose exec webapp composer install
 
 docker-compose exec webapp php artisan key:generate
 docker-compose exec webapp php artisan migrate:fresh --seed
-#docker-compose exec webapp php artisan passport:install
+docker-compose exec webapp php artisan passport:install
 docker-compose exec webapp php artisan storage:link
-
-#docker-compose exec webapp php artisan apidoc:generate
